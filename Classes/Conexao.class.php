8ed8a1c8135147a1359b10234ef35aba61fdfa12
blackmem712 <perotivictor712@gmail.com>
2023-03-27 21:28:31 -0400

    atualizações 27/03

---
+++ Classes/Conexao.class.php
@@ -0,0 +1,45 @@
+<?php
+final class Conexao
+{
+    private function _construct()
+    {
+
+    }
+    private static function conectar()
+    {
+        try {
+            if (!(defined('db_user'))) {
+                define('db_user', 'root');
+            }
+            if (!(defined('db_host'))) {
+                define('db_host', 'localhost');
+            }
+            if (!(defined('db_pass'))) {
+                define('db_pass', 'root');
+            }
+            if (!(defined('db_name'))) {
+                define('db_name', 'AulaTech');
+            }
+            if (!(defined('db_port'))) {
+                define('db_port', 3306);
+            }
+
+        
+        $db = new mysqli(db_host,db_user,db_pass,db_name,db_port);
+        return $db;
+    }
+    catch(mysqli_sql_exception $e)
+        {
+          echo 'problema ao conectar o banco de dados';
+          $e ->getMessage();
+        }
+    }
+
+
+    public static  function query($sql)
+    {
+      return self::conectar()->query($sql);
+    }
+
+}
+?>
---
+++ Classes/Crud.class.php
@@ -0,0 +1,9 @@
+<?php
+abstract class Crud
+{
+protected $tabela
+abstract function inserir();
+abstract function atualizar($campo,$id);
+
+}
+?>
