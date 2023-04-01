01a8f1ef6ec40d79f6ed3210f56ee03159f91e34
blackmem712 <perotivictor712@gmail.com>
2023-03-31 21:49:15 -0400

    atualizacao 27/03  2

--- Classes/Crud.class.php
+++ Classes/Crud.class.php
@@ -1,8 +1,8 @@
 <?php
 abstract class Crud
 {
-protected $tabela
-abstract function inserir();
+protected $tabela;
+ public abstract function inserir();
 abstract function atualizar($campo,$id);
 
 }
--- pacienteGer.php
+++ pacienteGer.php
@@ -11,61 +11,139 @@
 </head>
 
 <body>
-    <div class="container">
-        <form class="row g-3" action="<?php echo
-            htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post" enctype="multipart/form-data">
-            <div class="col-12">
-                <label for="txtNome" class="form-label">Nome</label>
-                <input type="text" class="form-control" id="txtNome" placeholder="Digite seu nome..." name="txtNome">
+    <header>
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">Navbar</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false"
+                    aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div class="navbar-nav">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        <a class="nav-link" href="#">Features</a>
+                        <a class="nav-link" href="#">Pricing</a>
+                        <a class="nav-link disabled">Disabled</a>
+                    </div>
+                </div>
             </div>
-            <div class="col-12">
-                <label for="txtEndereco" class="form-label">Endereço</label>
-                <input type="text" class="form-control" id="txtEndereco" placeholder="Digite seu endereço..."
-                    name="txtEndereco">
-            </div>
-            <div class="col-12">
-                <label for="txtBairro" class="form-label">Bairro</label>
-                <input type="text" class="form-control" id="txtBairro" placeholder="Digite seu bairro..."
-                    name="txtBairro">
-            </div>
-            <div class="col-md-6">
-                <label for="txtCidade" class="form-label">Cidade</label>
-                <input type="text" class="form-control" id="txtCidade" placeholder="Digite sua cidade..."
-                    name="txtCidade">
-            </div>
-            <div class="col-md-4">
-                <label for="sltEstado" class="form-label">Estado</label>
-                <select id="sltEstado" class="form-select" name="sltEstado">
-                    <option value="" selected hidden>Escolha...</option>
+        </nav>
+    </header>
+    <main>
+        <div class="container">
+            <?php
+            spl_autoload_register(function ($class) {
+                require_once "./Classes/{$class}.class.php";
+            });
+            if (filter_has_var(INPUT_POST, 'btnGravar')) {
+                if (isset($_FILES['filFoto'])) {
+                    $ext = strtolower(substr($_FILES['filfoto']['name'], -4));
+                    $nomeArq = md5(date("Y.m.d-H.i.s")) . $ext;
+                    $local = "imagesPac/";
+                    move_uploaded_file($_FILES['filFoto']['tmp_name'], $local . $nomeArq);
+                }
+            
 
-                </select>
-            </div>
-            <div class="col-md-2">
-                <label for="txtCep" class="form-label">Cep</label>
-                <input type="text" class="form-control" id="txtCep" name="txtCep">
-            </div>
-            <div class="col-12">
-                <label for="txtEmail" class="form-label">E-mail</label>
-                <input type="email" class="form-control" id="txtEmail" placeholder="Digite seu email..."
-                    name="txtEmail">
-            </div>
-            <div class="col-md-6">
-                <label for="txtNascimento" class="form-label">Nascimento</label>
-                <input type="date" class="form-control" id="txtNascimento" name="txtNascimento">
-            </div>
-            <div class="col-md-6">
-                <label for="txtCelular" class="form-label">Celular</label>
-                <input type="text" class="form-control" id="txtCelular" name="txtCelular">
-            </div>
-            <div class="col-12">
-                <label for="filFoto" class="form-label">Adicione sua Foto</label>
-                <input class="form-control" type="file" id="filFoto" name="filFoto">
-            </div>
-            <div class="col-12">
-                <button type="submit" class="btn btn-primary" name="btnGravar">Gravar</button>
-            </div>
-        </form>
-    </div>
+            $paciente = new Paciente();
+
+            $paciente->setNomePac(filter_input(INPUT_POST, 'txtNome'));
+            $paciente->setEnderecoPac(filter_input(INPUT_POST, 'txtEndereco'));
+            $paciente->setBairroPac(filter_input(INPUT_POST, 'txtBairro'));
+            $paciente->setCidadePac(filter_input(INPUT_POST, 'txtCidade'));
+            $paciente->setEstadoPac(filter_input(INPUT_POST, 'sltEstado'));
+            $paciente->setCepPac(filter_input(INPUT_POST, 'txtCep'));
+            $paciente->setNascimentoPac(filter_input(INPUT_POST, 'txtNascimento'));
+            $paciente->setEmailPac(filter_input(INPUT_POST, 'txtEmail'));
+            $paciente->setFotoPac($nomeArq);
+            $paciente->inserir();
+        }
+            ?>
+
+            <form class="row g-3" action="<?php echo
+                htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post" enctype="multipart/form-data">
+                <div class="col-12">
+                    <label for="txtNome" class="form-label">Nome</label>
+                    <input type="text" class="form-control" id="txtNome" placeholder="Digite seu nome..."
+                        name="txtNome">
+                </div>
+                <div class="col-12">
+                    <label for="txtEndereco" class="form-label">Endereço</label>
+                    <input type="text" class="form-control" id="txtEndereco" placeholder="Digite seu endereço..."
+                        name="txtEndereco">
+                </div>
+                <div class="col-12">
+                    <label for="txtBairro" class="form-label">Bairro</label>
+                    <input type="text" class="form-control" id="txtBairro" placeholder="Digite seu bairro..."
+                        name="txtBairro">
+                </div>
+                <div class="col-md-6">
+                    <label for="txtCidade" class="form-label">Cidade</label>
+                    <input type="text" class="form-control" id="txtCidade" placeholder="Digite sua cidade..."
+                        name="txtCidade">
+                </div>
+                <div class="col-md-4">
+                    <label for="sltEstado" class="form-label">Estado</label>
+                    <select id="sltEstado" class="form-select" name="sltEstado">
+                        <option value="" selected hidden>Escolha...</option>
+                        <option value="AC">Acre</option>
+                        <option value="AL">Alagoas</option>
+                        <option value="AP">Amapá</option>
+                        <option value="AM">Amazonas</option>
+                        <option value="BA">Bahia</option>
+                        <option value="CE">Ceará</option>
+                        <option value="DF">Distrito Federal</option>
+                        <option value="ES">Espírito Santo</option>
+                        <option value="GO">Goiás</option>
+                        <option value="MA">Maranhão</option>
+                        <option value="MT">Mato Grosso</option>
+                        <option value="MS">Mato Grosso do Sul</option>
+                        <option value="MG">Minas Gerais</option>
+                        <option value="PA">Pará</option>
+                        <option value="PB">Paraíba</option>
+                        <option value="PR">Paraná</option>
+                        <option value="PE">Pernambuco</option>
+                        <option value="PI">Piauí</option>
+                        <option value="RJ">Rio de Janeiro</option>
+                        <option value="RN">Rio Grande do Norte</option>
+                        <option value="RS">Rio Grande do Sul</option>
+                        <option value="RO">Rondônia</option>
+                        <option value="RR">Roraima</option>
+                        <option value="SC">Santa Catarina</option>
+                        <option value="SP">São Paulo</option>
+                        <option value="SE">Sergipe</option>
+                        <option value="TO">Tocantins</option>
+                    
+                    </select>
+                </div>
+                <div class="col-md-2">
+                    <label for="txtCep" class="form-label">Cep</label>
+                    <input type="text" class="form-control" id="txtCep" name="txtCep">
+                </div>
+                <div class="col-12">
+                    <label for="txtEmail" class="form-label">E-mail</label>
+                    <input type="email" class="form-control" id="txtEmail" placeholder="Digite seu email..."
+                        name="txtEmail">
+                </div>
+                <div class="col-md-6">
+                    <label for="txtNascimento" class="form-label">Nascimento</label>
+                    <input type="date" class="form-control" id="txtNascimento" name="txtNascimento">
+                </div>
+                <div class="col-md-6">
+                    <label for="txtCelular" class="form-label">Celular</label>
+                    <input type="text" class="form-control" id="txtCelular" name="txtCelular">
+                </div>
+                <div class="col-12">
+                    <label for="filFoto" class="form-label">Adicione sua Foto</label>
+                    <input class="form-control" type="file" id="filFoto" name="FilFoto">
+                </div>
+                <div class="col-12">
+                    <button type="submit" class="btn btn-primary" name="btnGravar">Gravar</button>
+                </div>
+            </form>
+        </div>
+    </main>
 
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
