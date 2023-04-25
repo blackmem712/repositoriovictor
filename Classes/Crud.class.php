878330892e8e24ca3c7c61b81edadd507d9bee99
blackmem712 <pedrotivictor712@gmail.com>
2023-04-24 22:13:58 -0400

    atualizacoes 24/04

--- Classes/Crud.class.php
+++ Classes/Crud.class.php
@@ -5,5 +5,16 @@ protected $tabela;
  public abstract function inserir();
 abstract function atualizar($campo,$id);
 
+public function listar()
+{
+    $sqlSelect = "select * from {$this ->tabela}";
+    return Conexao::query($sqlSelect);
+}
+public function buscar($campo,$id)
+{
+    $selectSql =  "SELECT * FROM {$this->tabela} WHERE $campo = $id";
+    $dados =  Conexao::query($selectSql);
+    return $dados->fetch_object();
+}
 }
 ?>
